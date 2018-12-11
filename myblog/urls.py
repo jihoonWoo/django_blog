58df1f8a3ee186b922243055112c9bc1ed6cfb48
Jihoon Woo <jihoonwoo6189@gmail.com>
2018-12-11 14:36:00 +1100

    edit URLconf, added homepage url

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 
+from myblog.views import HomeView	
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$',HomeView.as_view(), name='home'),
+
     url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
