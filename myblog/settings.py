18fd043fa110e51487785c6a6604a7c281cb5867
Jihoon Woo <jihoonwoo6189@gmail.com>
2018-12-18 21:48:53 +1100

    fix django 'WSGIRequest' object has no attribute 'user' issue

--- myblog/settings.py
+++ myblog/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'tagging.apps.TaggingConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
