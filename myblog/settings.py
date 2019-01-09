858ab522e7c30971fc20848230d06222d6f88b81
Jihoon Woo <jihoonwoo6189@gmail.com>
2019-01-09 18:37:13 +1100

    merge to master

--- myblog/settings.py
+++ myblog/settings.py
@@ -122,5 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]	#added
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]	#added
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 
